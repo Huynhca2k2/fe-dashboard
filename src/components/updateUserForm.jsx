c0e422dbcb744dc405e00d474b37b776d48db389
jin <2051010119huynh@ou.edu.vn>
2024-07-15 06:05:31 +0700

    test3 deloy

--- src/components/updateUserForm.jsx
+++ src/components/updateUserForm.jsx
@@ -87,6 +87,7 @@ const UpdateUserForm = ({ user, isRefesh, setIsRefesh }) => {
       return;
     } else {
       try {
+        setValue("image", urlImage);
         console.log(data);
         const newUser = await updateUser(userItem._id, data);
         setUserItem(newUser);
